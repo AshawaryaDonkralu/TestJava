3bd7a07f414e41d1822f6384a18b8e8cf7ce5ffe
AshwaryaDonkralu <dashwarya6@gmail.com>
2024-09-14 12:17:34 +0530

    Java Concept

---
+++ src/sept/ex_12092024/lab001.java
@@ -0,0 +1,23 @@
+package sept.ex_12092024;
+
+public class lab001 {
+	// main method
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("i am Ashwarya");
+		System.out.println("my first java class");
+		
+		/*There is also a print() method, which is similar to println().
+        The only difference is that it does not insert a new line at the end of the output*/
+		System.out.print("i am awesome");
+		System.out.print("print on same line");
+	}
+
+}
+// Every Java program has a class name which must match the filename, and that every program must contain the main() method.
+//System is a built-in Java class that contains useful members, such as out, which is short for "output". 
+//The println() method, short for "print line", is used to print a value to the screen (or a file).
+
+//Main is a method --Any code inside the main() method will be executed
+//System is a built-in Java class that contains useful members, such as out
+//println and print both are method
---
+++ src/sept/ex_12092024/lab002.java
@@ -0,0 +1,17 @@
+package sept.ex_12092024;
+
+public class lab002 {
+
+	public static void main(String[] args) {
+		//Print Numbers
+		System.out.println(2);
+		System.out.println(5);
+		
+        //mathematical calculations
+        System.out.println(2+2);
+		System.out.println(3*8);
+		System.out.println(5-2);
+		System.out.println(9/3);
+	}
+
+}
