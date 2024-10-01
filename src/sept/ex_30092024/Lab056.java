aaadf167e79309376226348a702148b3af3806cd
AshwaryaDonkralu <dashwarya6@gmail.com>
2024-10-01 20:01:46 +0530

    30th exercise

---
+++ src/sept/ex_30092024/Lab056.java
@@ -0,0 +1,36 @@
+package sept.ex_30092024;
+
+public class Lab056 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	    // // Functions
+        // Block of code which we can reuse.
+        // 1. Pre-built - Math.max() - Java guys
+        // 2. User created / defined
+
+
+        // Two component of the functions
+        // 1. Define
+        // 2. Call the function
+		  for (int i = 0; i < 100 ; i++) {
+	            greet();
+	        }
+	    }
+	    // Parameter == Arguments
+
+	    //  1.Without Parameters and Without Return Type
+	    // 2.Without Parameters but With Return Type
+	    // 3.With Parameters and Without Return Type
+	    // 4. With Parameters and With Return Type
+
+	    // Define
+	    // No Return Type and No Argument
+	    // 1.Without Parameters and Without Return Type
+	    public static void greet(){
+	        System.out.println("Hi");
+
+	}
+
+}
